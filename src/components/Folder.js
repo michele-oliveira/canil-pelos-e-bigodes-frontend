f06650f25515a5645a217e0eea5021fe014dc66c
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-01-12 17:00:31 -0300

    Melhorando o layout para telas muito grandes. Inserida uma largura máxima

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -3,7 +3,7 @@ import patinha from "../assets/images/patinhas.png";
 
 const Folder = () => {
   return (
-    <div className="relative bg-gray-100 mt-5 mx-4 p-6 rounded-lg flex flex-col md:flex-row items-center overflow-hidden">
+    <div className="relative flex flex-col mt-5 mx-4 p-6 md:flex-row items-center justify-center overflow-hidden rounded-lg bg-gray-100">
       <div 
         className="absolute inset-0 bg-cover bg-center" 
         style={{ 
@@ -13,7 +13,7 @@ const Folder = () => {
         }}
       ></div>
       
-      <div className="relative z-10 flex flex-col justify-center flex-grow md:mr-6 mb-6 md:mb-0">
+      <div className="relative z-10 flex flex-col flex-grow justify-center max-w-[1660px] md:mr-6 mb-6 md:mb-0">
         <h1 className="text-2xl flex justify-center md:text-3xl font-bold text-green-700 mb-4 text-center md:text-left pb-5 " style={{ fontFamily: 'Montserrat, serif' }}>
           Adote um Amigo e Transforme sua Vida!
         </h1>
