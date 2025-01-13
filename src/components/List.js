241a4aa08287b2a97841419362c85cddd41a6e06
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-01-13 18:05:16 -0300

    Criando componentes de Loading e List para melhorar UX

---
+++ src/components/List.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const List = ({
+  data,
+  component: Component,
+  emptyComponent: EmptyComponent,
+}) => {
+  if (data.length) {
+    return <Component />;
+  } else {
+    return <EmptyComponent />;
+  }
+};
+
+List.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.any).isRequired,
+  component: PropTypes.elementType.isRequired,
+  emptyComponent: PropTypes.elementType.isRequired,
+};
+
+export default List;
