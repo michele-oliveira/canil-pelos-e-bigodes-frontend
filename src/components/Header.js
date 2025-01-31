79494772ee94000c5f42dc7b3c5905cc3132a1a5
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-01-31 17:47:29 -0300

    Ajustando layout responsivo do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,95 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoMdMenu as MenuIcon, IoIosLogOut as LogoutIcon } from "react-icons/io";
+
 import logo from "../assets/images/logo.png";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="bg-gray-800 text-white py-4 px-6">
       <div className="flex items-center justify-between flex-wrap">
-        <div className="flex items-center mb-4 sm:mb-0">
+        <div className="flex items-center mb-0 sm:mb-0">
           <img
             src={logo}
             alt="Logo do Canil Pelos e Bigodes"
-            className="w-32 sm:w-20 sm:h-20 rounded-full mr-4"
+            className="w-12 sm:w-14 md:w-16 lg:w-20 rounded-full mr-4"
           />
-          <Link to="/" className="text-2xl sm:text-xl font-bold" style={{ fontFamily: 'Playfair Display, serif' }}>
+          <Link
+            to="/"
+            className="text-xl md:text-2xl font-bold"
+            style={{ fontFamily: "Playfair Display, serif" }}
+          >
             Pelos e Bigodes
           </Link>
         </div>
-        <nav className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 gap-5">
-          <Link to="/" className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300">
+        <button
+          className="md:hidden block text-white focus:outline-none"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <MenuIcon size={28} />
+        </button>
+        <nav className="hidden md:flex gap-5 space-x-1">
+          <Link
+            to="/"
+            className="text-base sm:text-sm md:text-md text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
             Home
           </Link>
-          <Link to="/login" className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300">
+          <Link
+            to="/login"
+            className="text-base sm:text-sm md:text-md text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
             Adote um Amigo
           </Link>
-          <Link to="/report" className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300">
+          <Link
+            to="/report"
+            className="text-base sm:text-sm md:text-md text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
             Informe uma Adoção
           </Link>
-          <Link to="/about" className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300">
-            Sobre Nós
+          <button
+            className="text-base sm:text-sm md:text-md text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
+            <span className="flex items-center">
+              <p className="mr-1">Sair</p>
+              <LogoutIcon className="text-lg" />
+            </span>
+          </button>
+        </nav>
+
+        <nav
+          className={`flex flex-col w-full gap-4 overflow-hidden md:hidden items-end font-semibold transition-all duration-300 ease-in-out transform ${
+            isMenuOpen ? "mt-2 max-h-40 opacity-100" : "mt-0 max-h-0 opacity-0"
+          }`}
+        >
+          <Link
+            to="/"
+            className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
+            Home
           </Link>
+          <Link
+            to="/login"
+            className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
+            Adote um Amigo
+          </Link>
+          <Link
+            to="/report"
+            className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
+            Informe uma Adoção
+          </Link>
+          <button
+            className="text-base sm:text-sm text-green-400 hover:text-green-500 transition-colors duration-300"
+          >
+            <span className="flex items-center">
+              <p className="mr-1">Sair</p>
+              <LogoutIcon className="text-lg" />
+            </span>
+          </button>
         </nav>
       </div>
     </header>
@@ -35,5 +97,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
