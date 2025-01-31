5bcf403d41833cce12abf1666994d58b14bfaf8c
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-01-31 17:47:47 -0300

    Movendo link 'Sobre nós' para o Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import { FaFacebookF, FaXTwitter, FaInstagram } from 'react-icons/fa6';
-import dogcat from "../assets/images/cachorro e gato 2.png";
+import dogCat from "../assets/images/cachorro e gato 2.png";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white flex flex-col md:flex-row items-center justify-between mt-5">
       <img
-        src={dogcat}
+        src={dogCat}
         alt="Cachorro e gato"
         className="w-32 md:w-44 rounded-lg mb-4 md:mb-0"
       />
@@ -22,12 +22,13 @@ const Footer = () => {
             <FaInstagram size={20} />
           </a>
         </div>
-        <p className="text-sm my-2">
-          <Link to="/" className="hover:text-green-400 transition-colors duration-300">
-            contato@canilpelosebigodes.com
-          </Link>
+        <p className="text-sm my-2 transition-colors duration-300">
+          contato@canilpelosebigodes.com
         </p>
-        <p className="mt-2 mb-4 md:mb-0 px-8 md:px-0 text-sm">
+        <Link to="/about" className="text-sm font-semibold hover:text-green-400 transition-colors duration-300">
+          Sobre nós
+        </Link>
+        <p className="my-4 md:mb-0 px-8 md:px-0 text-sm">
           &copy; {new Date().getFullYear()} Canil Pelos e Bigodes. Todos os direitos reservados.
         </p>
       </div>
