9f09fddef4444c128b30c7a3042ba299896476a7
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-02-05 20:24:47 -0300

    Implementando filtro de tipo de animal e efetuando algumas melhorias visuais nos cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,30 @@
 import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaFilter as FilterIcon } from "react-icons/fa";
 import toast from "../components/react-stacked-toast";
 import Loading from "./Loading";
 import List from "./List";
 import { listAnimals } from "../api/animals/animals.api";
 import { ITEMS_PER_PAGE } from "./constants/config";
+import { AnimalType } from "../enums/AnimalType";
 
 const Cards = () => {
   const [animals, setAnimals] = useState();
-  const [animalFilter] = useState(null);
+  const [animalFilter, setAnimalFilter] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(false);
+
+  const getAnimalType = (animalType) => {
+    switch (animalType) {
+      case AnimalType.CAT:
+        return "Gato";
+      case AnimalType.DOG:
+        return "Cachorro";
+      default:
+        return "";
+    }
+  };
 
   const fetchAnimals = async (page, animalType = null) => {
     try {
@@ -40,6 +53,27 @@ const Cards = () => {
       });
     } finally {
       setLoading(false);
+    }
+  };
+
+  const handleChangeAnimalFilter = (e) => {
+    if (e.target.value) {
+      if (Object.values(AnimalType)) {
+        setAnimalFilter(e.target.value);
+      } else {
+        const enumValues = Object.values(AnimalType);
+        const joinedValues =
+          enumValues.length === 2
+            ? `'${enumValues[0]}' or '${enumValues[1]}'`
+            : enumValues
+                .slice(0, -1)
+                .map((value) => `'${value}'`)
+                .join(", ") + `, or ${enumValues.at(-1)}`;
+        console.error(`animalType must be ${joinedValues}`);
+        setAnimalFilter(null);
+      }
+    } else {
+      setAnimalFilter(null);
     }
   };
 
@@ -77,6 +111,20 @@ const Cards = () => {
           data={animals}
           component={() => (
             <>
+              <div className="flex w-full mb-4 lg:mb-2 justify-end items-center">
+                <FilterIcon className="text-green-700" />
+                <div>
+                  <select
+                    value={animalFilter}
+                    onChange={handleChangeAnimalFilter}
+                    className="bg-transparent text-lg md:text-base font-semibold text-green-700"
+                  >
+                    <option value="">Todos os tipos</option>
+                    <option value={AnimalType.DOG}>Cachorros</option>
+                    <option value={AnimalType.CAT}>Gatos</option>
+                  </select>
+                </div>
+              </div>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 max-w-[1900px]">
                 {animals.map((animal) => (
                   <div
@@ -88,10 +136,15 @@ const Cards = () => {
                       alt={`Imagem de ${animal.name}`}
                       className="w-full h-64 object-cover mb-4"
                     />
-                    <h2 className="text-2xl font-bold text-green-700 pt-2">
+                    <h2 className="text-2xl font-bold leading-4 text-green-700 pt-2">
                       {animal.name}
                     </h2>
-                    <p className="text-gray-600 mb-2">Idade: {animal.age}</p>
+                    <p className="mb-2 text-sm text-gray-500">
+                      {getAnimalType(animal.type)}
+                    </p>
+                    <p className="text-gray-600 mb-2">
+                      Idade: {animal.age + (animal.age > 1 ? " meses" : " mês")}
+                    </p>
                     <p className="text-gray-600 mb-2">Raça: {animal.breed}</p>
                     <p className="text-gray-700 mb-4">{animal.description}</p>
                     <Link to={`/animal/${animal.id}`}>
